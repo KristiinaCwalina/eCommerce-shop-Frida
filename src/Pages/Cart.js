fcf38c09ad369cf33d9181000287f525fd423814
Kristiina Piip <kristiina.piip@gmail.com>
2020-07-02 11:23:54 +0200

    style of checkout page

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,52 +1,126 @@
 import React, { useEffect } from "react";
-import {addToCart} from "../actions/cartActions";
+import { addToCart } from "../actions/cartActions";
 import { useDispatch, useSelector } from "react-redux";
 
-function Cart (props) {
-    const cart =useSelector(state=>state.cart);
-    const {cartItems}= cart;
-    const productId = props.match.params.id;
-    const qty = props.location.search? Number(props.location.search.split("=")[1]):1;
-const dispatch = useDispatch();
-    useEffect(() => {
-        if(productId) {
-            dispatch(addToCart(productId, qty));
-        }
-    }, [])
-    return <div className="cart">
-        <div className = "cart-list"></div>
-        <ul className = "cart-list-container">
-            <li>
-                <h3>Shopping Cart</h3>
-                <div>Price </div>
-            </li>
-            {
-                cartItems.length === 0 ?
-                <div>Cart is empty</div> :
-                cartItems.map(item => 
-                    <div>
-                        <img src={item.image} alt="product"/>
-                        <div className="cart-name">
-                            <div>
-                                {item.name}
-                            </div>
-                            <div>
-                                Qty:
-                                <select>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                </select>
-                            </div>
+function Cart(props) {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const productId = props.match.params.id;
+  const qty = props.location.search
+    ? Number(props.location.search.split("=")[1])
+    : 1;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, []);
+  return (
+    <div className="container py-5">
+      <li className="cart-li">
+        <h3 className="display-5">SHOPPING CART</h3>
+      </li>
+
+      {cartItems.length === 0 ? (
+        <div>Cart is empty</div>
+      ) : (
+        cartItems.map((item) => (
+          <div>
+            <div className="table-responsive">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col" className="border-0 bg-light">
+                      <div className="p-2 px-3 text-uppercase">Product</div>
+                    </th>
+                    <th scope="col" className="border-0 bg-light">
+                      <div className="py-2 text-uppercase">Price</div>
+                    </th>
+                    <th scope="col" className="border-0 bg-light">
+                      <div className="py-2 text-uppercase">Quantity</div>
+                    </th>
+                    <th scope="col" className="border-0 bg-light">
+                      <div className="py-2 text-uppercase">Remove</div>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row" className="border-0">
+                      <div className="p-2">
+                        <img
+                          src={item.image}
+                          alt="product"
+                          width="70"
+                          className="img-fluid rounded shadow-sm"
+                        />
+                        <div className="ml-3 d-inline-block align-middle">
+                          <h5 className="mb-0">{item.name}</h5>
+                          <span className="text-muted font-weight-normal font-italic d-block">
+                            {item.category}
+                          </span>
                         </div>
-                        <div>
-                            {item.price}
-                        </div>
-                    </div>)
-            }
-        </ul>
-        <div className="cart-action"></div>
+                      </div>
+                    </th>
+                    <td className="border-0 align-middle">
+                      <strong>${item.price}</strong>
+                    </td>
+                    <td className="border-0 align-middle">
+                      <strong>
+                        {cartItems.reduce((a, c) => a + c.qty, 0)}
+                      </strong>
+                    </td>
+                    <td className="border-0 align-middle">
+                      <a href="#" className="text-dark">
+                        <i className="fa fa-trash"></i>
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div className="col-lg-6">
+              <div className="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
+                Order summary{" "}
+              </div>
+              <div className="p-4">
+                <ul className="list-unstyled mb-4">
+                  <li className="d-flex justify-content-between py-3 border-bottom">
+                    <strong class="text-muted">Order Subtotal </strong>
+                    <strong>
+                      ${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+                    </strong>
+                  </li>
+                  <li className="d-flex justify-content-between py-3 border-bottom">
+                    <strong class="text-muted">Shipping and handling</strong>
+                    <strong>Free</strong>
+                  </li>
+                  <li className="d-flex justify-content-between py-3 border-bottom">
+                    <strong class="text-muted">Tax</strong>
+                    <strong>$0.00</strong>
+                  </li>
+                  <li className="d-flex justify-content-between py-3 border-bottom">
+                    <strong class="text-muted">Total</strong>
+                    <h5 className="font-weight-bold">
+                      ${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+                    </h5>
+                  </li>
+                </ul>
+                <a
+                  href="#"
+                  className="btn btn-dark rounded-pill py-2 btn-block"
+                  disabled={cartItems.length === 0}
+                >
+                  Procceed to checkout
+                </a>
+              </div>
+            </div>
+          </div>
+        ))
+      )}
     </div>
+  );
 }
 
 export default Cart;
